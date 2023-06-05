6885add1a8a9f4776ea079cc519b165d6947a5c1
Сергей Арефьев <Simpson6515@yandex.ru>
2023-06-05 19:38:19 +0500

    add funcion filtered data

--- src/utils.py
+++ src/utils.py
@@ -9,3 +9,8 @@ def get_data():
     return data
 
 
+def get_filtered_data(data, filter_empty_from=False):
+    data = [x for x in data if "state" in x and x["state"] == "EXCUTED"]
+    if filter_empty_from:
+        data = [x for x in data if "from" in x]
+    return data
