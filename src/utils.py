d0f136cbbc14387244037eb38874fc97befa981e
Сергей Арефьев <Simpson6515@yandex.ru>
2023-06-08 00:55:29 +0500

    with open(Path(__file__).parent / 'operations.json', 'r', encoding='utf-8') as file:

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 def get_data():
-    with open('operations.json', 'r', encoding='utf-8') as file:
+    with open(Path(__file__).parent / 'operations.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
